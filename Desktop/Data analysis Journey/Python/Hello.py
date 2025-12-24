e61cc7b37c227ad57ccfa0a33f740e4df15aa0f3
Rogue-weeb <efeomohwovo02@gmail.com>
2025-12-24 09:38:10 +0100

    sub code_login page

---
+++ Desktop/Data analysis Journey/Python/Hello.py
@@ -0,0 +1,13 @@
+print("Hello, world")
+
+name = input("Enter your name: ").strip()
+print(f"Hello, {name}")
+
+password = input( "Enter password:")
+
+if password == 1020: 
+    print("correct")
+else: 
+    print('incorrect') 
+
+
