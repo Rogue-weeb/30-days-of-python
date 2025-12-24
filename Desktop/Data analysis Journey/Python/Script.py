9d94c7b15d19e2f24a1bd0929a90b0425284004b
Rogue-weeb <efeomohwovo02@gmail.com>
2025-12-24 06:19:21 +0100

    Day 1: Basic Python exercises - input, conditionals, and arithmetic operations

---
+++ Desktop/Data analysis Journey/Python/Script.py
@@ -0,0 +1,29 @@
+print("Hello world, watch me become fucking amazing")
+print()
+print()
+# Day 1 - 30 days of python
+name = input("What is your name: ")
+print('Hello', name)
+age = input("How old are you: ")
+print(age)
+if int(age) > 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+print()
+print()
+
+#i am now writing on the same line with end=''
+print('Hello again world', end='')
+print('this is not a new line')
+print()
+print()
+
+#Checking mathematical Calculations
+print(3 + 2) #addition
+print(3 - 2) #subtraction
+print(3 * 2) #multiplication
+print(6 / 2) #division
+print(6 % 2) #modulus
+print(6 // 2) #floor division
+print(6 ** 2) #exponentiation   
